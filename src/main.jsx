0c8bd57d1bd981f1f34cce0658e569b68141f64e
Mostafa Ghorab <vetghorab@gmail.com>
2024-10-14 04:35:04 +0300

    adding ErrorFallbackComponent

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,16 @@
-import { StrictMode } from 'react';
+import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { ErrorBoundary } from 'react-error-boundary';
 import App from './App.jsx';
+import ErrorFallback from './ui/ErrorFallback.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <React.StrictMode>
+    <ErrorBoundary
+      FallbackComponent={ErrorFallback}
+      onReset={() => window.location.replace('/')}
+    >
+      <App />
+    </ErrorBoundary>
+  </React.StrictMode>
 );

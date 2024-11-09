2cbfbdcb19b2b453d1299859c88d9da55f3469b1
Mostafa Ghorab <vetghorab@gmail.com>
2024-11-09 18:08:36 +0200

    adding test data to login page

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import SpinnerMini from '../../ui/SpinnerMini';
 import { useLogin } from './useLogin';
 
 function LoginForm() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('test@gmail.com');
+  const [password, setPassword] = useState('88888888');
 
   const { login, isLogging } = useLogin();
 
